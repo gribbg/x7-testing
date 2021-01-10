bb37fef704097397a169a712f11215cea4012869
Glenn Gribble <git@gribbelus.com>
2021-01-10 12:01:49 -0500

    New style shell tools

--- x7/testing/shell_tools.py
+++ x7/testing/shell_tools.py
@@ -1,10 +1,20 @@
-"""Devtools entry points to be called from Python shell"""
+"""
+    Shell tools for x7.testing
+
+    Usage: from x7.shell import *
+"""
+
+__all__ = ['maketests']
+
+import _sitebuiltins
+_sitebuiltins.Quitter
 
 
 def maketests(module=None, really=False, force=False):
     """Generate/update tests for python module"""
     import sys
     import x7.testing.maketests.maketests as maketests_mod
+
     if not module:
         print('Help: maketests(module-name, really=False, force=False)')
         print('Example: maketests("x7.sample.needs_test", True)')
@@ -21,25 +31,3 @@ def maketests(module=None, really=False, force=False):
             maketests_mod.main()
         finally:
             sys.argv = saved
-
-
-def tools():
-    """Help for tools"""
-    print('Help for tools')
-    for n in _all_tools():
-        f = globals()[n]
-        if callable(f):
-            print('  %s - %s' % (n, f.__doc__))
-
-
-def _all_tools():
-    return [k for k in sorted(globals().keys()) if not k.startswith(('_', 'do_import'))]
-
-
-def do_import(other_globals: dict):
-    """Import appropriate symbols into <other_globals>"""
-
-    for n in _all_tools():
-        other_globals[n] = globals()[n]
-    other_globals['__all__'] = _all_tools()
-    print("Tools imported.  Commands: %s" % ', '.join(t + '()' for t in _all_tools()))
